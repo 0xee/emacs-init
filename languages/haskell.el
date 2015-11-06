f837dd819e8385d406c4763556de0e62748bf9ee
0xee <lukas@0xee.eu>
2015-11-06 07:44:44 +0100

    added haskell mode

--- languages/haskell.el
+++ languages/haskell.el
@@ -3,10 +3,24 @@
 
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
 (custom-set-variables
   '(haskell-process-suggest-remove-import-lines t)
   '(haskell-process-auto-import-loaded-modules t)
   '(haskell-process-log t))
+
+;; (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+;;   (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
+;;   (add-to-list 'exec-path my-cabal-path))
+
+;; (add-to-list 'load-path "~/.cabal/share/ghc-mod-5.0.1.2")
+
+;; (autoload 'ghc-init "ghc" nil t)
+;; (autoload 'ghc-debug "ghc" nil t)
+
+(add-hook 'haskell-mode-hook (lambda ()
+;                               (ghc-init)
+                               (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+                               (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+                               )
+          )

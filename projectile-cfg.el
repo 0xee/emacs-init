b5815e1477910a0a41d5a1621a1964b53ac9579a
Lukas Schuller <lukas.schuller@ims.co.at>
2015-11-06 09:54:29 +0100

    Fixed C-c p a, C-c p A

--- projectile-cfg.el
+++ projectile-cfg.el
@@ -4,7 +4,11 @@
 
 (projectile-global-mode t)
 
+(defun projectile-ag-regexp ()
+  (interactive)
+  (let ((current-prefix-arg '(4))) (call-interactively 'projectile-ag)))
+
 (define-key projectile-mode-map (kbd "C-c p s") 'projectile-switch-project)
 (define-key projectile-mode-map (kbd "C-c c") 'projectile-compile-project)
-(define-key projectile-mode-map (kbd "C-c p a") 'ag-project)
-(define-key projectile-mode-map (kbd "C-c p A") 'ag-project-regexp)
+(define-key projectile-mode-map (kbd "C-c p a") 'projectile-ag)
+(define-key projectile-mode-map (kbd "C-c p A") 'projectile-ag-regexp)

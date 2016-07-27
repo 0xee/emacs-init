6cff4a9ba17ac8ed181f636ac1d673a7bb85529f
Lukas Schuller <lukas.schuller@ims.co.at>
2016-07-27 08:27:07 +0200

    Added jabber-cfg.el

---
+++ jabber-cfg.el
@@ -0,0 +1,9 @@
+(setq jabber-invalid-certificate-servers '("srv-voip-04"))
+
+(package-require 'jabber)
+
+
+(set-variable 'jabber-history-enabled t)
+(set-variable 'jabber-history-muc-enabled t)
+
+(set-variable 'jabber-roster-line-format " %c %-25n %u %-8s  %S")

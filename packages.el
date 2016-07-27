08dad4a44afc1c73e03432dba97c6425d687ce5d
Lukas Schuller <lukas.schuller@ims.co.at>
2016-07-27 08:20:42 +0200

    Added function package-require

--- packages.el
+++ packages.el
@@ -3,9 +3,25 @@
 ;;; Package management
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Make sure a package is installed
+(defun package-require (package)
+  "Install a PACKAGE unless it is already installed
+or a feature with the same name is already active.
+
+Usage: (package-require 'package)"
+  ; try to activate the package with at least version 0.
+  (package-activate package '(0))
+  ; try to just require the package. Maybe the user has it in his local config
+  (condition-case nil
+      (require package)
+    ; if we cannot require it, it does not exist, yet. So install it.
+    (error (package-install package))))
+
+
+
 (add-to-list 'package-archives
-         '("melpa" . "http://melpa-stable.milkbox.net/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
+
+;(set-variable 'package-load-list (quote ((el-get) all)))
 
 (package-initialize)
-
-(load-file "~/.emacs.d/elpa/el-get-5.1/el-get.el")
